30e5822552e5c4c79186984c355fb3e0693a8f7d
Abhay <abhayak6053@gmail.com>
2025-12-18 16:36:32 -0500

    FIrst edit

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,18 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session && req.session.accessToken) {
+      // user is authenticated
+      next();
+    } else {
+      // user is NOT authenticated
+      res.status(401).json({
+        message: "Unauthorized access"
+      });
+    }
+  });
+  
  
 const PORT =5000;
 
